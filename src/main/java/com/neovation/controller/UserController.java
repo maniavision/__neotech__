e27a0ff855df39c5d079ec9dbfef7767ce38be14
Fortune Niama <fortune_niama@outlook.com>
2025-11-16 20:05:19 -0500

    .

--- src/main/java/com/neovation/controller/UserController.java
+++ src/main/java/com/neovation/controller/UserController.java
@@ -2,9 +2,12 @@ package com.neovation.controller;
 
 import com.neovation.model.User;
 import com.neovation.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,10 +18,14 @@ import java.util.Map;
 //@CrossOrigin
 public class UserController {
 
-    private final UserService userService;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    public UserController(UserService userService) {
+    private final UserService userService;
+    final private PasswordEncoder passwordEncoder;
+
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/me")
@@ -37,5 +44,29 @@ public class UserController {
 
         // Return the URL in the response body
         return ResponseEntity.ok().body(Map.of("profileImageUrl", publicUrl));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        log.info("User attempting to update user with ID: {}", id);
+        return userService.findUserById(id)
+                .map(user -> {
+                    user.setFirstName(updatedUser.getFirstName());
+                    user.setLastName(updatedUser.getLastName());
+                    user.setCompanyName(updatedUser.getCompanyName());
+                    user.setEmail(updatedUser.getEmail());
+                    user.setPhone(updatedUser.getPhone());
+//                    if (!updatedUser.getPassword().isBlank()) {
+//                        user.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+//                    }
+//                    user.setRole(updatedUser.getRole()); // Optional: only if changing role
+                    User savedUser = this.userService.saveUser(user);
+                    log.info("User successfully updated user ID: {}", id);
+                    return ResponseEntity.ok(savedUser);
+                })
+                .orElseGet(() -> {
+                    log.warn("User failed to update - user not found with ID: {}", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 }
